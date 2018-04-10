2c7e819e290d8648ed9fb48c7878bc28c23390c1
Ubuntu <ubuntu@ip-172-31-23-157.ap-northeast-1.compute.internal>
2018-04-10 12:08:11 +0000

    m

---
+++ worktool/find.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#This file is auto-generated.Edit it at your own peril
+if [ "$#" -eq 1 ]
+then
+grep --color=always -n -w -E "$1" --include=*.h --include=*.cc -r *
+elif [ "$#" -eq 2 ]
+then 
+grep --color=always -n -w -E "$1" --include=*.h --include=*.cc -r * | grep -E "$2"
+fi
