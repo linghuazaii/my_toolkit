eceaa18ef478ad5fcac4468890e332a56c324290
linghuazaii <charlesliu.cn.bj@gmail.com>
2018-05-24 17:40:16 +0800

    m

--- worktool/find.sh
+++ worktool/find.sh
@@ -2,8 +2,8 @@
 #This file is auto-generated.Edit it at your own peril
 if [ "$#" -eq 1 ]
 then
-grep --color=always -n -w -E "$1" --include=*.h --include=*.hpp --include=*.cc --include=*.cpp -r *
+grep --color=always -n -w -E "$1" --include=*.h --include=*.hpp --include=*.cc --include=*.cpp --include=*.c -r *
 elif [ "$#" -eq 2 ]
 then 
-grep --color=always -n -w -E "$1" --include=*.h --include=*.hpp --include=*.cc --include=*.cpp -r * | grep -E "$2"
+grep --color=always -n -w -E "$1" --include=*.h --include=*.hpp --include=*.cc --include=*.cpp --include=*.c -r * | grep -E "$2"
 fi
