14d5f1c680ab444ce0688744830c9aad4ad6e9c7
linghuazaii <charlesliu.cn.bj@gmail.com>
2018-07-17 11:29:25 +0800

    up

---
+++ worktool/valgrind.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#This file is auto-generated.Edit it at your own peril
+valgrind --tool=memcheck --trace-children=yes --log-file=valgrind.log --demangle=yes --leak-check=full --leak-resolution=high --show-reachable=no --undef-value-errors=no --track-origins=no 
